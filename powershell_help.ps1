290cd3bd8a77f921aec753eeaf4eb794a9f5bf03
Fredy-SSA <alin@zizo.ro>
2020-04-27 14:10:51 +0300

    ps

--- powershell_help.ps1
+++ powershell_help.ps1
@@ -1,11 +1,41 @@
 ﻿Get-Command 
 
-Get-Command  | measure
+Get-Command  | Measure-Object
+Get-Command  | Measure
+
 
 Get-Command *ip*ad*
+Get-Command *IPAddress*
 
 Get-Command -Module NetTCPIP
 
-get-help 
+get-help Test-NetConnection
+get-help Test-NetConnection -Examples 
+get-help Test-NetConnection -Online 
+help Test-NetConnection -ShowWindow
+help Test-NetConnection -full
 
+help Get-EventLog -ShowWindow
+
+#example
+Test-NetConnection 
+Test-NetConnection -Port 80 -InformationLevel Detailed
+Test-NetConnection -ComputerName www.contoso.com -InformationLevel Detailed
+
+Update-Help -Force
 Show-Command
+
+#find Ping
+
+Get-Command test*con*
+
+get-help Test-Connection -ShowWindow 
+
+Test-Connection -ComputerName "."
+Test-Connection  "." -Count 1 
+Test-Connection  "." -Count 1 | fl
+Test-Connection  "." -Count 1 | ft
+
+# show last 10 entry
+Get-EventLog 
+
