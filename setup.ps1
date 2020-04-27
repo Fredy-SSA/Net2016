20b5c12022aaf9ce5a95bd2b9eab3e86ba024194
Fredy-SSA <alin@zizo.ro>
2020-04-27 11:07:08 +0300

    all

---
+++ setup.ps1
@@ -0,0 +1,11 @@
+mkdir C:\GitNet
+cd C:\GitNet
+
+git init
+
+git pull https://github.com/Fredy-SSA/Net2016
+
+git remote add origin https://github.com/Fredy-SSA/Net2016
+
+git config --global user.name "UserGit"
+git config --global user.email email@adresa.ro
