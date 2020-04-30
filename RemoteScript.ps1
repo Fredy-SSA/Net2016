ad2cfa46b24bd95aabd75246c00de13976bfe6ee
Fredy-SSA <alin@zizo.ro>
2020-04-30 16:13:32 +0300

    rs

---
+++ RemoteScript.ps1
@@ -0,0 +1,10 @@
+﻿$Running =  Get-VM | where  state -like "Running"
+
+$credential = Get-Credential
+
+Invoke-Command -VMName $Running.Name -ScriptBlock {
+
+Get-Service 
+
+} -Credential $credential
+
