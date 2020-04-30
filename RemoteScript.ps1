71c3cda90d31b11f76c572b05309f77bd0f56c4d
Fredy-SSA <alin@zizo.ro>
2020-04-30 16:27:55 +0300

    ps

--- RemoteScript.ps1
+++ RemoteScript.ps1
@@ -8,3 +8,26 @@ Get-Service
 
 } -Credential $credential
 
+
+# V2
+
+$Running =  Get-VM | where  state -like "Running" 
+ 
+$credential = Get-Credential 
+
+$test = Invoke-Command -VMName $Running.Name -ScriptBlock { 
+
+     #   Get-Service  
+
+        Get-EventLog -LogName Application -Newest 15
+ 
+        } -Credential $credential 
+
+$test | ? PSComputerName -Like "20741B-LON-DC1"  | fl
+
+$test > c:\test.txt
+
+ise c:\test.txt
+
+Invoke-Command -ComputerName 
+
